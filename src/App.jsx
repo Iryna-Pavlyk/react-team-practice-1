de17dd311b903c03dd9c4c8120baa10d2274cc1c
Iryna-Pavlyk <145845654+Iryna-Pavlyk@users.noreply.github.com>
2024-04-07 15:44:55 +0300

    task 1

--- src/App.jsx
+++ src/App.jsx
@@ -7,13 +7,22 @@ import {
   CryptoHistory,
   ForbesList,
 } from 'components';
+import articles from './data/article.json';
 
 export const App = () => {
   return (
     <Section>
       <Container>
         <Heading title="Task 1 Blog Card" bottom />
-        <BlogCard />
+        <BlogCard
+          poster={articles.poster}
+          tag={articles.tag}
+          title={articles.title}
+          description={articles.description}
+          name={articles.name}
+          avatar={articles.avatar}
+          postedAt={articles.postedAt}
+        />
 
         <Heading title="Task 2 Statistics" top bottom />
         <Statistics title="Main Statistics" />
